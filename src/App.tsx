c1000256daa2fba80dda15870984700e8603ab98
Kritic000 <greencontinenttg@gmail.com>
2026-02-23 01:57:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -976,7 +976,7 @@ const styles: Record<string, React.CSSProperties> & {
   },
 
   title: {
-    fontSize: 27,
+    fontSize: 26,
     fontWeight: 700,
     letterSpacing: -0.2,
     background: "linear-gradient(90deg, #1E2A32 0%, #e9c46a 100%)",
@@ -1439,6 +1439,7 @@ const styles: Record<string, React.CSSProperties> & {
     fontWeight: 650,
   },
 };
+
 
 
 
