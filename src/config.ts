2a30c29d64a395fbf6302d0151f0fd97b9df07e8
Kritic000 <greencontinenttg@gmail.com>
2026-02-25 17:51:15 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://script.google.com/macros/s/AKfycbyrlKoVcFpQQhio0wPQEgvevz6NUqq4d3r3IUDatCk-pYkUcbRnY3nI8dZdygeNS7ncNw/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbw0APFzQg73THSJWH8ipkd4jgc7tUO8qzU9OPGLJYgDVtep-A5-pEiEE2xGNNa3rE1ToQ/exec";
 export const API_TOKEN = "Kjhytccb18@";
