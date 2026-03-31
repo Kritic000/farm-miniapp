1bc8909bbeed44080acf1ff04a3e1412130f2e60
Kritic000 <greencontinenttg@gmail.com>
2026-03-31 21:28:40 +0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
       return;
     }
 
-    // CORS (на всякий случай)
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
