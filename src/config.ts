c621f66d532dc9b031952db74e90ec4be7c34f61
Kritic000 <greencontinenttg@gmail.com>
2026-04-07 14:30:04 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,8 @@
-const GS_API_URL =
-  "https://script.google.com/macros/s/AKfycbw5ljb1uQHfgfTad0BG3O7Rx9yKlsM6Qo3Hso3AexjXP410oUX_PwSp15eogowchqRspw/exec";
+const GS_API_URL = "https://script.google.com/macros/s/AKfycbw5ljb1uQHfgfTad0BG3O7Rx9yKlsM6Qo3Hso3AexjXP410oUX_PwSp15eogowchqRspw/exec";
 
-const GS_API_TOKEN = "Kjhytccb18@";
+export const API_TOKEN = "Kjhytccb18@";
 
 export const API_PRODUCTS_URL = `${GS_API_URL}?action=products`;
-
 export const API_ORDER_URL = GS_API_URL;
-
-export const API_ORDERS_URL = `${GS_API_URL}?action=orders&token=${encodeURIComponent(
-  GS_API_TOKEN
-)}`;
-
+export const API_ORDERS_URL = `${GS_API_URL}?action=orders&token=${encodeURIComponent(API_TOKEN)}`;
 export const API_CANCEL_URL = GS_API_URL;
-
-export const API_TOKEN = GS_API_TOKEN;
