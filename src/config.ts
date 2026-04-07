883c46371ea7b503608838e0dd23bbc10f54b95c
Kritic000 <greencontinenttg@gmail.com>
2026-04-07 14:40:39 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_PRODUCTS_URL = "/api/products.php";
-export const API_ORDER_URL = "/api/order.php";
-export const API_ORDERS_URL = "/api/orders.php";
-export const API_CANCEL_URL = "/api/cancel-order.php";
+export const API_PRODUCTS_URL = "/api/products";
+export const API_ORDER_URL = "/api/order";
+export const API_ORDERS_URL = "/api/orders";
+export const API_CANCEL_URL = "/api/cancel-order";
